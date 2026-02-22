97adc489dbe271849bf8777ea3c7c496f9cf79cd
Siraj Paravoor <143482878+sparavoor@users.noreply.github.com>
2026-02-22 14:04:07 +0530

    Fix: Updated connection string and Prisma config for stable production connection

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     migrations: {
         path: "prisma/migrations",
         seed: "ts-node ./prisma/seed.ts",
+        url: process.env.DIRECT_URL,
     },
     datasource: {
         url: process.env.DATABASE_URL,
