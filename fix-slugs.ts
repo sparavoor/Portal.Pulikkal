b0f1b743f568dd6a669687b5de694ec5f8f1e6d7
Siraj Paravoor <143482878+sparavoor@users.noreply.github.com>
2026-02-22 10:30:46 +0530

    Fix build errors by removing utility script and adding prisma generate to postinstall

--- fix-slugs.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-    const sectors = await prisma.sector.findMany()
-    for (const s of sectors) {
-        if (!s.slug) await prisma.sector.update({ where: { id: s.id }, data: { slug: Math.random().toString(36).substring(2, 8) } })
-    }
-}
-main()
